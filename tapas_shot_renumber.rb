2b2e72fea8cc0996a82587860c920583b1398f5f
Avdi Grimm <avdi@avdi.codes>
2019-11-25 21:09:35 -0500

    store function instead of boolean

--- tapas_shot_renumber.rb
+++ tapas_shot_renumber.rb
@@ -5,12 +5,12 @@ shot\(\d*\)  # e.g. 'shot(23)' or 'shot()'
 /x
 
 def do_in_fence(line, state)
-  state[:in_fenced] = false if (line =~ /^```/)
+  state[:thing_to_do] = method(:do_out_of_fence) if (line =~ /^```/)
   line 
 end
 
 def do_out_of_fence(line, state)
-  state[:in_fenced] = true if (line =~ /^```/)
+  state[:thing_to_do] = method(:do_in_fence) if (line =~ /^```/)
   line.sub(SHOT_NUMBER_PATTERN) {
     "shot(#{state[:shot_number] += 1})"
   }
@@ -18,13 +18,9 @@ end
 
 ARGF
   .each_line
-  .each_with_object({shot_number: 0, in_fenced: false})
+  .each_with_object({shot_number: 0, thing_to_do: method(:do_out_of_fence)})
   .map { |line, state|
-    if state[:in_fenced] then 
-      do_in_fence(line, state)
-    else 
-      do_out_of_fence(line, state)
-    end
+    state[:thing_to_do].call(line, state)
   }
   .each(&method(:puts))
 
