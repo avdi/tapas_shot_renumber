1405f29a0d8834871966ea524c9d8a227089ec9f
Avdi Grimm <avdi@avdi.codes>
2019-11-25 22:00:01 -0500

    rename business logic functions

--- tapas_shot_renumber.rb
+++ tapas_shot_renumber.rb
@@ -4,30 +4,30 @@ shot\(\d*\)  # e.g. 'shot(23)' or 'shot()'
 (?=\s.*-->)  # must be followed by '-->'
 /x
 
-def do_in_fence(next_shot_number)
+def in_fenced_code_block(next_shot_number)
   return ->(line) {
     if (line =~ /^```/)
-      [line, do_out_of_fence(next_shot_number)]
+      [line, in_body(next_shot_number)]
     else
-      [line, do_in_fence(next_shot_number)] 
+      [line, in_fenced_code_block(next_shot_number)] 
     end
   }
 end
 
-def do_out_of_fence(next_shot_number) 
+def in_body(next_shot_number) 
   return ->(line) {
-    return [line, do_in_fence(next_shot_number)] if line =~ /^```/
+    return [line, in_fenced_code_block(next_shot_number)] if line =~ /^```/
     if line.sub!(SHOT_NUMBER_PATTERN, "shot(#{next_shot_number})")
-      [line, do_out_of_fence(next_shot_number + 1)]
+      [line, in_body(next_shot_number + 1)]
     else
-      [line, do_out_of_fence(next_shot_number)]
+      [line, in_body(next_shot_number)]
     end
   }
 end
 
 ARGF
   .each_line
-  .reduce(do_out_of_fence(1)) { |thing_to_do, line|
+  .reduce(in_body(1)) { |thing_to_do, line|
     line, thing_to_do = thing_to_do.call(line)
     puts line
     thing_to_do
