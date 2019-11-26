5c12040541c686475502560ba06632bf677d8a53
Avdi Grimm <avdi@avdi.codes>
2019-11-25 21:29:30 -0500

    store shot # in the stack instead of in state

--- tapas_shot_renumber.rb
+++ tapas_shot_renumber.rb
@@ -4,21 +4,29 @@ shot\(\d*\)  # e.g. 'shot(23)' or 'shot()'
 (?=\s.*-->)  # must be followed by '-->'
 /x
 
-def do_in_fence(line, state)
-  state[:thing_to_do] = method(:do_out_of_fence) if (line =~ /^```/)
-  line 
+def do_in_fence(next_shot_number)
+  return ->(line, state) {
+    state[:thing_to_do] = do_out_of_fence(next_shot_number) if (line =~ /^```/)
+    line 
+  }
 end
 
-def do_out_of_fence(line, state)
-  state[:thing_to_do] = method(:do_in_fence) if (line =~ /^```/)
-  line.sub(SHOT_NUMBER_PATTERN) {
-    "shot(#{state[:shot_number] += 1})"
+def do_out_of_fence(next_shot_number) 
+  return ->(line, state) {
+    output_line = line
+    if line =~ SHOT_NUMBER_PATTERN
+      next_shot_number = next_shot_number + 1
+      output_line = line.sub(SHOT_NUMBER_PATTERN, "shot(#{next_shot_number})")
+      state[:thing_to_do] = do_out_of_fence(next_shot_number)
+    end
+    state[:thing_to_do] = do_in_fence(next_shot_number) if (line =~ /^```/)
+    output_line
   }
 end
 
 ARGF
   .each_line
-  .each_with_object({shot_number: 0, thing_to_do: method(:do_out_of_fence)})
+  .each_with_object({thing_to_do: do_out_of_fence(0)})
   .map { |line, state|
     state[:thing_to_do].call(line, state)
   }
