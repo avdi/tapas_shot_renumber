8a125aa9d84ccda771599257b2179cf8c1608655
Avdi Grimm <avdi@avdi.codes>
2019-11-25 21:01:55 -0500

    extract if branches

--- tapas_shot_renumber.rb
+++ tapas_shot_renumber.rb
@@ -1,21 +1,29 @@
-shot_number_pattern = /
+SHOT_NUMBER_PATTERN = /
 (?<=\<!--\s) # Must be preceded by '<!--'
 shot\(\d*\)  # e.g. 'shot(23)' or 'shot()'
 (?=\s.*-->)  # must be followed by '-->'
 /x
+
+def do_in_fence(line, state)
+  state[:in_fenced] = false if (line =~ /^```/)
+  line 
+end
+
+def do_out_of_fence(line, state)
+  state[:in_fenced] = true if (line =~ /^```/)
+  line.sub(SHOT_NUMBER_PATTERN) {
+    "shot(#{state[:shot_number] += 1})"
+  }
+end
 
 ARGF
   .each_line
   .each_with_object({shot_number: 0, in_fenced: false})
   .map { |line, state|
     if state[:in_fenced] then 
-      state[:in_fenced] = false if (line =~ /^```/)
-      line 
+      do_in_fence(line, state)
     else 
-      state[:in_fenced] = true if (line =~ /^```/)
-      line.sub(shot_number_pattern) {
-        "shot(#{state[:shot_number] += 1})"
-      }
+      do_out_of_fence(line, state)
     end
   }
   .each(&method(:puts))
