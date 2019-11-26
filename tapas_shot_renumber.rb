649e5cc5baba5413311e4aa5f66634de041493f6
Avdi Grimm <avdi@avdi.codes>
2019-11-25 20:50:39 -0500

    get rid of xor

--- tapas_shot_renumber.rb
+++ tapas_shot_renumber.rb
@@ -8,10 +8,11 @@ ARGF
   .each_line
   .each_with_object({shot_number: 0, in_fenced: false})
   .map { |line, state|
-    state[:in_fenced] ^= (line =~ /^```/)
     if state[:in_fenced] then 
+      state[:in_fenced] = false if (line =~ /^```/)
       line 
     else 
+      state[:in_fenced] = true if (line =~ /^```/)
       line.sub(shot_number_pattern) {
         "shot(#{state[:shot_number] += 1})"
       }
