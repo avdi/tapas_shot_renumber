729bdce42e9a305f4e84415cbcae2de7a958a1e4
Avdi Grimm <avdi@users.noreply.github.com>
2021-08-20 11:50:02 -0500

    CRLF->LF, add shebang

--- tapas_shot_renumber.rb
+++ tapas_shot_renumber.rb
@@ -1,12 +1,13 @@
+#!/usr/bin/env ruby
 SHOT_NUMBER_PATTERN = /
 (?<=\<!--\s) # Must be preceded by '<!--'
 shot\(\d*\)  # e.g. 'shot(23)' or 'shot()'
 (?=\s.*-->)  # must be followed by '-->'
 /x
 ENTER_CODE_BLOCK_PATTERN = /^```/
-EXIT_CODE_BLOCK_PATTERN  = /^```/
+EXIT_CODE_BLOCK_PATTERN = /^```/
 
-def in_body(next_shot_number) 
+def in_body(next_shot_number)
   look_for_shots = ->(line) {
     if line.sub!(SHOT_NUMBER_PATTERN, "shot(#{next_shot_number})")
       [line, in_body(next_shot_number + 1)]
@@ -19,13 +20,12 @@ def in_body(next_shot_number)
   look_for_shots
 end
 
-
 def in_fenced_code_block(next_shot_number)
   look_for_end = ->(line) {
     if (line =~ EXIT_CODE_BLOCK_PATTERN)
       [line, in_body(next_shot_number)]
     else
-      [line, look_for_end] 
+      [line, look_for_end]
     end
   }
   look_for_end
@@ -34,14 +34,12 @@ end
 ARGF
   .each_line
   .reduce(in_body(1)) { |thing_to_do, line|
-    begin 
-      line, thing_to_do = thing_to_do.call(line)
-    rescue => e 
-      warn "Unable to process line: " + line
-      raise
-    end
-    puts line
-    thing_to_do
-  }
-
-
+  begin
+    line, thing_to_do = thing_to_do.call(line)
+  rescue => e
+    warn "Unable to process line: " + line
+    raise
+  end
+  puts line
+  thing_to_do
+}
